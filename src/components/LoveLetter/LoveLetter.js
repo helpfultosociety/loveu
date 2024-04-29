7e113fab2d8d176b8e88618bebc747695dd33584
helpfultosociety <89498633+helpfultosociety@users.noreply.github.com>
2024-04-30 00:35:49 +0530

    Add files via upload

---
+++ src/components/LoveLetter/LoveLetter.js
@@ -0,0 +1,47 @@
+import React, { useState, useRef } from 'react';
+import './LoveLetter.css';
+import audioFile from './Audio.mp3';
+
+const LoveLetter = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [isFullSize, setIsFullSize] = useState(false);
+  const audioRef = useRef(null);
+
+  const handleOpenLetter = () => {
+    setIsOpen(true);
+    setTimeout(() => {
+      setIsFullSize(true);
+      // Ensuring audio play is directly a result of this user interaction
+      if (audioRef.current) {
+        audioRef.current.play()
+          .then(() => console.log("Playback succeeded"))
+          .catch(e => console.error("Playback failed:", e));
+      }
+    }, 800);
+  };
+
+  const handleCloseLetter = () => {
+    setIsFullSize(false);
+    setTimeout(() => {
+      if (audioRef.current) {
+        audioRef.current.pause();
+      }
+      setIsOpen(false);
+    }, 800);
+  };
+
+  return (
+    <div className={`envelope ${isOpen ? 'open' : ''}`} onClick={!isFullSize ? handleOpenLetter : handleCloseLetter}>
+      <div className="flap"></div>
+      <div className="body"></div>
+      <div className={`letter ${isFullSize ? 'fullSize' : ''}`}>
+        I Love U Bujjii &#10084; <br />
+        <br />
+        
+      </div>
+      <audio ref={audioRef} src={audioFile} onError={(e) => console.error('Audio error:', e.message)} />
+    </div>
+  );
+};
+
+export default LoveLetter;
